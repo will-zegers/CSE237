cc54b436a5af91ca3a284e0a5b50cfb9bee63e48
Will Zegers <will.zegers@gmail.com>
2016-10-03 14:48:48 -0700

    Working 11-tap FIR filter code

--- project1/fir11_baseline/fir.cpp
+++ project1/fir11_baseline/fir.cpp
@@ -11,12 +11,30 @@
 */
 
 #include "fir.h"
+
 void fir (
   data_t *y,
   data_t x
   ) {
 	coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0,53};
 	// Write your code here
+	static data_t shift_reg[N];
+	acc_t acc;
+	data_t data;
+	int i;
+
+	acc = 0;
+	for (i = N-1; i >= 0; i--) {
+		if (i == 0) {
+			shift_reg[0] = x;
+			data = x;
+		} else {
+			shift_reg[i] = shift_reg[i-1];
+			data = shift_reg[i];
+		}
+		acc += data * c[i];
+	}
+	*y = acc;
 }
 
 
---
+++ project1/fir11_baseline/script.tcl
@@ -0,0 +1,20 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 2015 Xilinx Inc. All rights reserved.
+############################################################
+open_project fir11_baseline
+set_top fir
+add_files ../Workspaces/cse237c_wzegers/project1/fir11_baseline/fir.cpp
+add_files ../Workspaces/cse237c_wzegers/project1/fir11/fir.h
+add_files -tb ../Workspaces/cse237c_wzegers/project1/fir11/fir_test.cpp
+add_files -tb ../Workspaces/cse237c_wzegers/project1/fir11/input.dat
+add_files -tb ../Workspaces/cse237c_wzegers/project1/fir11/out.gold.dat
+open_solution "solution1"
+set_part {xc7z020clg484-1}
+create_clock -period 10 -name default
+#source "./fir11_baseline/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design
+export_design -format ip_catalog
